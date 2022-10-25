65a2b65ea20739c4a6b5a64de90442a96d4e4632
MCabal77 <marccabal77@gmail.com>
2022-10-25 04:48:34 -0700

    Update experiences section

--- src/components/ExperienceItem.jsx
+++ src/components/ExperienceItem.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function ExperienceItem({title, icon, description}) {
   return (
-    <div className=' bg-MAIN_COLOR hover:bg-MAIN_COLOR_DARKER hover:-translate-y-2 transition transform p-5 rounded-md'>
+    <div className=' bg-MAIN_COLOR hover:bg-MAIN_COLOR_DARKER hover:-translate-y-2 transition transform p-5 rounded-md flex flex-col flex-wrap'>
       <div className='text-STROKE_COLOR w-6 h-6 mb-3'>
         {icon}
       </div>
       <h1 className='font-semibold text-lg text-STROKE_COLOR mb-1'>{title}</h1>
-      <p className='text-md text-BG_COLOR '>{description}</p>
+      <p className='text-md text-BG_COLOR  flex-'>{description}</p>
     </div>
   )
 }
--- src/data/experiences.jsx
+++ src/data/experiences.jsx
@@ -1,21 +1,27 @@
 import {GoCode} from 'react-icons/go'
 import {FaHandHoldingHeart} from 'react-icons/fa'
 import {AiOutlineDatabase} from 'react-icons/ai'
+import {IoLogoJavascript} from 'react-icons/io'
 
 export default [
   {
     title: 'Frontend Development',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nemo iure ad! Quas distinctio adipisci delectus, recusandae eligendi aspernatur minima.',
+    description: 'React, Angular, Vue, Tailwind, Bootstrap, Next, Vite',
     icon: <FaHandHoldingHeart className='w-full h-full'/>, 
   }, 
   {
     title: 'Backend Development',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nemo iure ad! Quas distinctio adipisci delectus, recusandae eligendi aspernatur minima.',
+    description: 'Ruby on Rails, Django, Node.js, Nest.js, Express.js',
     icon: <GoCode className='w-full h-full'/>,
   },
   {
     title: 'Data Management',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nemo iure ad! Quas distinctio adipisci delectus, recusandae eligendi aspernatur minima.',
+    description: 'AWS, Google Cloud ',
     icon: <AiOutlineDatabase className='w-full h-full'/>,
+  },
+  {
+    title: 'Languages',
+    description: 'JavaScript, Python, Java, SQL, NoSQL, C#, Ruby',
+    icon: <IoLogoJavascript className='w-full h-full'/>,
   }
 ]
