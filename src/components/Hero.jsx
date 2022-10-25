7979c9a6cc610b0670eeb0b8419c8daf86a50916
MCabal77 <marccabal77@gmail.com>
2022-10-25 03:30:56 -0700

    Add a typewriter effect to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,32 @@
 import React from "react";
+import TypeWriter from "typewriter-effect";
 
 function Hero() {
   return (
-    <div id='hero' className="flex items-center justify-center flex-col py-20">
+    <div id="hero" className="flex items-center justify-center flex-col py-20">
       <div className="text-center">
         <h1 className="text-2xl md:text-4xl mb-1 md:mb-3 text-H_COLOR font-semibold">
           &lt;marc_cabal&gt;
         </h1>
         <p className="text-md md:text-xl max-w-md mb-3 text-P_COLOR">
-          A full-stack web developer.
+          I'm a
+          <div className=" text-TERTIARY_COLOR">
+            <TypeWriter
+              options={{
+                strings: ["web developer", "creator", "designer", "programmer"],
+                autoStart: true,
+                loop: true,
+                pauseFor: 2500,
+              }}
+            />
+          </div>
         </p>
-        <a href='#works' className="inline-block px-8 py-3 border border-transparent text-base font-medium rounded-md text-BTN_TXT bg-BTN_COLOR hover:bg-BTN_COLOR_DARKER md:text-md">See Works</a>
+        <a
+          href="#works"
+          className="inline-block px-8 py-3 border border-transparent text-base font-medium rounded-md text-BTN_TXT bg-BTN_COLOR hover:bg-BTN_COLOR_DARKER md:text-md"
+        >
+          See Works
+        </a>
       </div>
     </div>
   );
