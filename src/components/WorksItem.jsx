7a138a4f5c18a621f19b6e66e445a10a6bfe9e1c
MCabal77 <marccabal77@gmail.com>
2022-11-05 00:44:32 -0700

    Fix a bug with alignment
    A bug was introduced when adding a description to works. Fixed the alignment of items.

--- src/components/WorksItem.jsx
+++ src/components/WorksItem.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 
-function WorksItem({ imageUrl, title, description, tech, workUrl}) {
+function WorksItem({ imageUrl, title, description, tech, workUrl }) {
   return (
-    <a href={workUrl} target='_blank' className=" bg-TERTIARY_COLOR rounded-lg overflow-hidden group hover:bg-BTN_COLOR_DARKER transform transition duration-500 hover:scale-105">
+    <a
+      href={workUrl}
+      target="_blank"
+      className=" bg-TERTIARY_COLOR rounded-lg overflow-hidden group hover:bg-BTN_COLOR_DARKER transform transition duration-500 hover:scale-105 flex flex-col justify-between"
+    >
       <img
         src={imageUrl}
         alt={title}
         className="w-full h-36 md:h-48 object-cover"
       />
-      <div className="text-STROKE_COLOR p-5 w-full">
+      <div className="text-STROKE_COLOR p-5 w-full flex flex-col grow justify-between">
         <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
           {title}
         </h3>
         <p className="mb-2 md:mb-3">{description}</p>
         <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
-          {tech.map(item => (
-            <span key={item} className='inline-block px-2 py-1 bg-H_COLOR rounded-md'>{item}</span>
+          {tech.map((item) => (
+            <span
+              key={item}
+              className="inline-block px-2 py-1 bg-H_COLOR rounded-md"
+            >
+              {item}
+            </span>
           ))}
         </p>
       </div>
--- src/data/works.js
+++ src/data/works.js
@@ -1,16 +1,16 @@
 export default [
   {
-    title: 'Landing Page Visualization',
-    description: 'blah blah blah',
-    imageUrl: 'https://i.postimg.cc/y631q32V/webpage-1.png',
-    tech: ['HTML', 'CSS'],
-    workUrl: 'https://mcabal77.github.io/landing-page-visualization/',
-  },
-  {
     title: 'Banking App React Visualization',
-    description: 'blah blah blah',
+    description: 'First modern design implementation on a website. This is for proof of concept and currently is not set up with any databases or APIs.',
     imageUrl: 'https://i.postimg.cc/1XmfBm5g/Bank-React1.png',
     tech: ['HTML', 'CSS', 'React JS', 'Vite', 'Tailwind CSS'],
     workUrl: 'https://polite-wisp-a0b2ed.netlify.app/',
+  },
+  {
+    title: 'Landing Page Visualization',
+    description: 'A landing page built solely on HTML5 and CSS3. Currently does not support mobile responsiveness.',
+    imageUrl: 'https://i.postimg.cc/y631q32V/webpage-1.png',
+    tech: ['HTML', 'CSS'],
+    workUrl: 'https://mcabal77.github.io/landing-page-visualization/',
   },
 ]
